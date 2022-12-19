c15a2c1ec1f48f470ae7fc60645a7340bdb146c5
juanpc19 <juan.perez@iesnervion.es>
2022-12-19 09:28:45 +0100

    ej 7 hecho

--- Tema5-EjerciciosArraysClase/src/ejercicio/Ejercicio7.java
+++ Tema5-EjerciciosArraysClase/src/ejercicio/Ejercicio7.java
@@ -6,56 +6,75 @@ import java.util.Scanner;
 public class Ejercicio7 {
 
 	public static void main(String[] args) {
-		
-		//array del participante
-		int primitivaParticipante[] = new int [6];
-		
-		//array del sorteo
-		int primitivaSorteo[] = new int [6];
-		
+
+		//Declaro variable donde guardare el valor de la cuenta del contador
+		int contador = 0;
+
+		// array del participante
+		int primitivaParticipante[] = new int[6];
+
+		// array del sorteo
+		int primitivaSorteo[] = new int[6];
+
+		//Creo un escaner, lo nombro dogma y lo importo
 		Scanner dogma = new Scanner(System.in);
-		
-		for (int i=0; i<=primitivaParticipante.length-1; i++) {
-				
-			int numeroRandom=(int) (Math.random()*49);
-				
-			if (numeroRandom==0) {
+
+		//bucle para dar valor random entre 1 y 49 a array primitivaParticipante
+		for (int i = 0; i <= primitivaParticipante.length - 1; i++) {
+
+			//Creacion de variable que contiene el valor del numero random creado para la iteracion
+			int numeroRandom = (int) (Math.random()*50);
+
+			//Establezco la condicion si numeroRandom igual a 0 
+			if (numeroRandom == 0) {
+				//que de cumplirse hara un continue repitiendo la iteracion del bucle con otro numeroRandom
 				continue;
-				
+
 			} else {
-				primitivaParticipante[i]=numeroRandom;
+				//De no cumplirse la condicion anterior doy a la posicion i del array el valor de numeroRandom
+				primitivaParticipante[i] = numeroRandom;
 			}
 		}
+
 		
-		System.out.println(Arrays.toString(primitivaParticipante));
-		
-		for (int i=0; i<=primitivaSorteo.length-1; i++) {
-			
-			int numeroRandom=(int) (Math.random()*49);
-				
-			if (numeroRandom==0) {
+		//bucle para dar valor random entre 1 y 49 a array primitivaSorteo
+		for (int i = 0; i <= primitivaSorteo.length - 1; i++) {
+
+			int numeroRandom = (int) (Math.random()*50);
+
+			if (numeroRandom == 0) {
 				continue;
-				
+
 			} else {
-				primitivaSorteo[i]=numeroRandom;
+				primitivaSorteo[i] = numeroRandom;
 			}
 		}
-		
-		//para testeo
-		System.out.println(Arrays.toString(primitivaSorteo));
-		
+
+		// para testeo
+		// System.out.println(Arrays.toString(primitivaSorteo));
+
 		Arrays.sort(primitivaSorteo);
-		
-		//para testeo
-		System.out.println(Arrays.toString(primitivaSorteo));
-		
-		for (int posicion : primitivaParticipante) {
-			
-			Arrays.binarySearch(primitivaSorteo, primitivaSorteo[0], primitivaSorteo.length-1,  posicion);	
-			
-		//Arrays.binarySearch(primitivaSorteo, posicion);	
+
+		// para testeo
+		// System.out.println(Arrays.toString(primitivaSorteo));
+
+		/*prueba busqueda binaria
+		 * for (int posicion : primitivaParticipante) {
+		 * 
+		 * Arrays.binarySearch(primitivaSorteo, primitivaSorteo [0],
+		 * primitivaSorteo.length-1, posicion);
+		 * 
+		 * // Arrays.binarySearch(primitivaSorteo, posicion); }
+		 */
+
+		for (int i = 0; i < primitivaSorteo.length - 1; ++i) {
+			if (primitivaSorteo[i] == primitivaParticipante[i]) {
+				contador++;
+			}
 		}
-		
+
+		System.out.println(contador);
+
 		dogma.close();
 	}
 
